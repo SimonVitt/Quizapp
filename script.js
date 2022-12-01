05ae8e5199cb11825478a9eb34be43c9a569660d
Simon Vitt <ttivnomis@gmail.com>
2022-12-02 00:21:36 +0100

    question and button work

---
+++ script.js
@@ -0,0 +1,51 @@
+let contentQuiz = [
+    {
+        "question": "Wie alt bin ich?",
+        "answers": [18, 19, 20, 21],
+        "solution": 1
+    },
+    {
+        "question": "Wie alt ist Karl?",
+        "answers": [8, 19, 30, 21],
+        "solution": 0
+    },
+    {
+        "question": "Wie alt ist Luis?",
+        "answers": [18, 49, 70, 21],
+        "solution": 3
+    },
+    {
+        "question": "Wie alt ist Lukas?",
+        "answers": [18, 19, 20, 21],
+        "solution": 2
+    }
+];
+
+let currentQuestion = 0;
+
+function init(){
+    document.getElementById('numberQuestions').innerHTML = contentQuiz.length;
+    document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = contentQuiz[currentQuestion];
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answers'][0];
+    document.getElementById('answer_2').innerHTML = question['answers'][1];
+    document.getElementById('answer_3').innerHTML = question['answers'][2];
+    document.getElementById('answer_4').innerHTML = question['answers'][3];
+}
+
+function answer(selection){
+    let selectedAnswerNumber = selection.slice(-1);
+    if(selectedAnswerNumber - 1 == contentQuiz[currentQuestion]['solution']){
+        document.getElementById(selection).parentNode.parentNode.classList.add('bg-success');
+    }else{
+        let idRightAnswer = `answer_${contentQuiz[currentQuestion]['solution'] + 1}`;
+        document.getElementById(selection).parentNode.parentNode.classList.add('bg-danger');
+        document.getElementById(idRightAnswer).parentNode.parentNode.classList.add('bg-success')
+    }
+    document.getElementById('nextBtn').disabled = false;
+}
